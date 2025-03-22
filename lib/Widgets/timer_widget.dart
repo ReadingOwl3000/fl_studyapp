1afec968d2a72d38c3a5cae91deaeefd9e0ccc37
Reading Owl <ladydragon7@web.de>
2025-03-22 13:55:04 +0100

    add line of text to tell whether it's focus or break

--- lib/Widgets/timer_widget.dart
+++ lib/Widgets/timer_widget.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:fl_studyapp/main.dart';
+import 'package:provider/provider.dart';
+import 'package:fl_studyapp/timer.dart';
 
 class TimerWidget {
   static Widget buildTime(Duration duration) {
@@ -7,14 +10,26 @@ class TimerWidget {
     final minutes = twoDigits(duration.inMinutes.remainder(60));
     final seconds = twoDigits(duration.inSeconds.remainder(60));
     double scale = 4;
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
+    return Column(
       children: [
-        Text(hours, textScaler: TextScaler.linear(scale)),
-        Text(":", textScaler: TextScaler.linear(scale)),
-        Text(minutes, textScaler: TextScaler.linear(scale)),
-        Text(":", textScaler: TextScaler.linear(scale)),
-        Text(seconds, textScaler: TextScaler.linear(scale)),
+        Text(
+          (Provider.of<StudyTimer>(
+                listen: true,
+                navigatorKey.currentContext!,
+              ).isFocus)
+              ? "Focus Timer"
+              : "Break Timer",
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(hours, textScaler: TextScaler.linear(scale)),
+            Text(":", textScaler: TextScaler.linear(scale)),
+            Text(minutes, textScaler: TextScaler.linear(scale)),
+            Text(":", textScaler: TextScaler.linear(scale)),
+            Text(seconds, textScaler: TextScaler.linear(scale)),
+          ],
+        ),
       ],
     );
   }
