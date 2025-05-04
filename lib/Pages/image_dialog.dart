e563c007b2553d43982eac0dd931450a8a686280
Reading Owl <ladydragon7@web.de>
2025-05-04 09:25:18 +0200

    error handling if an image does not exist (anymore)

--- lib/Pages/image_dialog.dart
+++ lib/Pages/image_dialog.dart
@@ -21,7 +21,28 @@ class ImageDialog extends StatefulWidget {
 }
 
 class _ImageDialogState extends State<ImageDialog> {
-  Widget imageOption(title, image) {
+  Future<Widget> imageOption(
+    String title,
+    ImageProvider image, {
+    filepath,
+  }) async {
+    bool error = false;
+    // print(error);
+    // try {
+    //   print(image);
+    //   var file = File(path: image);
+    //   if ()
+    // } catch (e) {
+    //   print("error: $e");
+    //   error = true;
+    // }
+    // FlutterError.onError = (details) {};
+    if (filepath != null) {
+      if (!(await File(filepath).exists())) {
+        error = true;
+      }
+    }
+
     return SimpleDialogOption(
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -31,52 +52,76 @@ class _ImageDialogState extends State<ImageDialog> {
             height: 70,
             width: 70,
             decoration: BoxDecoration(
-              image: DecorationImage(image: image, fit: BoxFit.contain),
+              image:
+                  error
+                      ? null
+                      : DecorationImage(image: image, fit: BoxFit.contain),
             ),
+            child: error ? Text("error: image not found") : SizedBox.shrink(),
           ),
         ],
       ),
       onPressed: () {
-        ImageDialog.currentImage = image;
-        Provider.of<StudyTimer>(
-          listen: false,
-          navigatorKey.currentContext!,
-        ).notify();
+        if (!error) {
+          ImageDialog.currentImage = image;
+          Provider.of<StudyTimer>(
+            listen: false,
+            navigatorKey.currentContext!,
+          ).notify();
+        }
       },
     );
   }
 
+  Future<List<Widget>> buildWidgetList() async {
+    List<Widget> list = [
+      await imageOption("Pink flower", AssetImage("assets/image_flower.jpg")),
+      await imageOption(
+        "Tree at a river",
+        AssetImage("assets/flower_tree.jpg"),
+      ),
+      await imageOption("Waterfall", AssetImage("assets/waterfall.jpg")),
+      for (int i = 0; i < MyHomePageState.imageList.length; i++)
+        await imageOption(
+          "from files",
+          FileImage(File(MyHomePageState.imageList[i])),
+          filepath: MyHomePageState.imageList[i],
+        ),
+      TextButton(
+        onPressed: () => Navigator.of(context).pop(),
+        child: Text("Close"),
+      ),
+    ];
+
+    return list;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SimpleDialog(
-      title: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text("Pick a background image"),
-          IconButton(
-            onPressed: () async {
-              await ImagePicker().pick();
-              setState(() {});
-            },
-            icon: Icon(Icons.add_a_photo_outlined),
-          ),
-        ],
-      ),
-      children: [
-        imageOption("Pink flower", AssetImage("assets/image_flower.jpg")),
-        imageOption("Tree at a river", AssetImage("assets/flower_tree.jpg")),
-        imageOption("Waterfall", AssetImage("assets/waterfall.jpg")),
-        for (int i = 0; i < MyHomePageState.imageList.length; i++)
-          imageOption(
-            "from files",
-            FileImage(File(MyHomePageState.imageList[i])),
-          ),
-
-        TextButton(
-          onPressed: () => Navigator.of(context).pop(),
-          child: Text("Close"),
-        ),
-      ],
+    return FutureBuilder<List<Widget>>(
+      future: buildWidgetList(), // Call the Future method
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        } else {
+          return SimpleDialog(
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("Pick a background image"),
+                IconButton(
+                  onPressed: () async {
+                    await ImagePicker().pick();
+                    setState(() {});
+                  },
+                  icon: const Icon(Icons.add_a_photo_outlined),
+                ),
+              ],
+            ),
+            children: snapshot.data ?? [], // Use the data from the Future
+          );
+        }
+      },
     );
   }
 }
