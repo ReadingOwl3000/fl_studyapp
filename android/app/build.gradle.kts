91c488bb7bc06f0aa8e20c88f165f762b5f753d2
Reading Owl <ladydragon7@web.de>
2025-08-04 14:12:49 +0200

    renames

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.fl_studyapp"
+    namespace = "de.reading_owl_productions.fl_studyapp"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.fl_studyapp"
+        applicationId = "de.reading_owl_productions.fl_studyapp"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
